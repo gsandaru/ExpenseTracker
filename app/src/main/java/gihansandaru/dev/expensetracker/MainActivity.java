6e89e74254df5902d5543490dd9e3fc4a3dabd4e
Gihan Sandaru <gihans@IronOne.Local>
2018-10-05 12:57:54 +0530

    fragment continer

--- app/src/main/java/gihansandaru/dev/expensetracker/MainActivity.java
+++ app/src/main/java/gihansandaru/dev/expensetracker/MainActivity.java
@@ -1,23 +1,14 @@
 package gihansandaru.dev.expensetracker;
 
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.Date;
-import java.util.List;
-
 import gihansandaru.dev.expensetracker.Adapters.ExpenseAdapter;
-import gihansandaru.dev.expensetracker.models.Expense;
 import gihansandaru.dev.expensetracker.viewmodels.ExpensesViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,38 +27,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        spinner = findViewById(R.id.dropdown_Expenses_category);
-        txtAmount = findViewById(R.id.text_Amount);
-        btnNewExpense = findViewById(R.id.btnAddExpense);
-        txtNoExpensesToDisplay = findViewById(R.id.txtNoExpensesToDisplay);
-        recyclerView = findViewById(R.id.Recyclerview_main);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        expensesViewModel = ViewModelProviders.of(this).get(ExpensesViewModel.class);
-
-        expensesViewModel.getListLiveData().observe(this, new Observer<List<Expense>>() {
-            @Override
-            public void onChanged(@Nullable List<Expense> expenses) {
-                if( expenses.size() == 0 ){
-                    recyclerView.setVisibility(View.GONE);
-                }else{
-                    recyclerView.setVisibility(View.VISIBLE);
-                    txtNoExpensesToDisplay.setVisibility(View.GONE);
-                    recyclerView.bringToFront();
-                    expenseAdapter = new ExpenseAdapter(getApplicationContext(),expenses);
-
-                    recyclerView.setAdapter(expenseAdapter);
-                }
-            }
-        });
-
-        btnNewExpense.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String amt = txtAmount.getText().toString();
-                Expense expense = new Expense(0,spinner.getSelectedItem().toString(),Long.valueOf(amt),new Date());
-                expensesViewModel.insertExpense(expense);
-            }
-        });
+//        spinner = findViewById(R.id.dropdown_Expenses_category);
+//        txtAmount = findViewById(R.id.text_Amount);
+//        btnNewExpense = findViewById(R.id.btnAddExpense);
+//        txtNoExpensesToDisplay = findViewById(R.id.txtNoExpensesToDisplay);
+//        recyclerView = findViewById(R.id.Recyclerview_main);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//
+//        expensesViewModel = ViewModelProviders.of(this).get(ExpensesViewModel.class);
+//
+//        expensesViewModel.getListLiveData().observe(this, new Observer<List<Expense>>() {
+//            @Override
+//            public void onChanged(@Nullable List<Expense> expenses) {
+//                if( expenses.size() == 0 ){
+//                    recyclerView.setVisibility(View.GONE);
+//                }else{
+//                    recyclerView.setVisibility(View.VISIBLE);
+//                    txtNoExpensesToDisplay.setVisibility(View.GONE);
+//                    recyclerView.bringToFront();
+//                    expenseAdapter = new ExpenseAdapter(getApplicationContext(),expenses);
+//
+//                    recyclerView.setAdapter(expenseAdapter);
+//                }
+//            }
+//        });
+//
+//        btnNewExpense.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String amt = txtAmount.getText().toString();
+//                Expense expense = new Expense(0,spinner.getSelectedItem().toString(),Long.valueOf(amt),new Date());
+//                expensesViewModel.insertExpense(expense);
+//            }
+//        });
     }
 }
