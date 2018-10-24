0c17677396c16ac10556711058a42012ae617ea5
Gihan Sandaru <gihandevacc@gmail.com>
2018-10-25 00:17:37 +0530

    Temp

---
+++ app/src/main/java/gihansandaru/dev/expensetracker/Adapters/DayViewExpenseAdapter.java
@@ -0,0 +1,30 @@
+package gihansandaru.dev.expensetracker.Adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DayViewExpenseAdapter extends RecyclerView.Adapter<DayViewExpenseAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
